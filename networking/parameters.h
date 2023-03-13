cba6dbed7e48f58f7934ef25779f44cc5640661d
Arman Zand <arman.zand@protonmail.com>
2023-03-13 01:17:45 +0000

    added todo in parameters.h

--- networking/parameters.h
+++ networking/parameters.h
@@ -11,4 +11,9 @@ bool keepTrying = true;
 int waitRetry = 1000;
 const int prefixSize = sizeof(int);
 
+//TODO:
+//1. listener status events
+//2. correct destruction
+//3. protobuf
+
 #endif //CLEAN_SOCKETS_PARAMETERS_H
