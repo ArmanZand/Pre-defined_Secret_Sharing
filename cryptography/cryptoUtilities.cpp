dbdbde8b454a4e2e6a341bf0a13ab906c6ce0e2b
Arman Zand <arman.zand@protonmail.com>
2023-03-15 01:36:58 +0000

    bcrypt added to cmake and added csprng function in cryptography subdir

---
+++ cryptography/cryptoUtilities.cpp
@@ -0,0 +1,15 @@
+//
+// Created by arman on 15/03/2023.
+//
+
+#include "cryptoUtilities.h"
+#include <Windows.h>
+#include <bcrypt.h>
+
+
+void cryptoUtilities::secureRandom(unsigned char *buffer, size_t length) {
+    BCRYPT_ALG_HANDLE hProvider;
+    BCryptOpenAlgorithmProvider(&hProvider, BCRYPT_RNG_ALGORITHM, NULL, 0);
+    BCryptGenRandom(hProvider, buffer, length, 0);
+    BCryptCloseAlgorithmProvider(hProvider, 0);
+}
---
+++ cryptography/cryptoUtilities.h
@@ -0,0 +1,15 @@
+//
+// Created by arman on 15/03/2023.
+//
+
+#ifndef CLEAN_SOCKETS_CRYPTOUTILITIES_H
+#define CLEAN_SOCKETS_CRYPTOUTILITIES_H
+
+
+class cryptoUtilities {
+private:
+    void secureRandom(unsigned char * buffer, size_t length);
+};
+
+
+#endif //CLEAN_SOCKETS_CRYPTOUTILITIES_H
